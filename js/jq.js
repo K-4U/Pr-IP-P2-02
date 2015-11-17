9b8d7d70e07690ac2f3644ded174dcfcf3e5054f
Koen Beckers <koen@k-4u.nl>
2015-11-17 15:45:31 +0100

    More work on the high fidel. Sidebar done

--- js/jq.js
+++ js/jq.js
@@ -1,1 +1,8 @@
-
+$(document).ready(function () {
+    $('label.tree-toggler').click(function () {
+        $(this).parent().children('ul.tree').toggle(300);
+    });
+    $('label.tree-toggler').each(function(index, element){
+        $(this).parent().children('ul.tree').toggle(300);
+    });
+});
