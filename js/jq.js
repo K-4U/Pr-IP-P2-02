c5d411d05de702364d3a6b59531ab44164baa046
Koen Beckers <koen@k-4u.nl>
2015-11-18 11:20:56 +0100

    Made navbar and menu stick on scroll.

--- js/jq.js
+++ js/jq.js
@@ -5,4 +5,24 @@ $(document).ready(function () {
     $('label.tree-toggler').each(function(index, element){
         $(this).parent().children('ul.tree').toggle(300);
     });
+
+    var menu = $('.menu');
+    var content = $('.content');
+    var sidebar = $('.sidebar');
+    var origOffsetY = menu.offset().top;
+
+    function scroll() {
+        if ($(window).scrollTop() >= origOffsetY) {
+            menu.addClass('sticky');
+            content.css('padding-top', origOffsetY);
+        } else {
+            menu.removeClass('sticky');
+            content.css('padding-top', "");
+        }
+
+        sidebar.css('top', (menu.offset().top - $(window).scrollTop()) + menu.outerHeight());
+    }
+
+    document.onscroll = scroll;
+
 });
