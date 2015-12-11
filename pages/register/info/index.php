d1f697fd9b1db5b70b3efdf2ebd45de3c4fa35bc
ALE.Scheps <steak@live.nl>
2015-12-11 15:49:36 +0100

    Progress on register, got to info, added reCaptcha although it doesn't work yet "domain key"-problem

--- pages/register/info/index.php
+++ pages/register/info/index.php
@@ -1,11 +1,39 @@
 <?php
 
-Class registerInfo extends cmsPage{
+Class registerInfo extends cmsPage {
 
-    function parse(){
+    function parse() {
 
+        if($this->user->isLoggedIn()) {
+            header("location: " . baseurl(""));
+        } else {
+            if(isset($_POST['validateCode'])) {
+                if($_SESSION['emailCode'] == $_POST['emailVerificationCode']) {
+
+                    $sqlQuestion = "SELECT id, question FROM security_questions";
+
+                    $questionResult = $this->db->query($sqlQuestion);
+
+                    $questions = Array();
+                    $questions = $this->db->fetchAllAssoc($questionResult);
+                    $this->website->assign("questions", $questions);
+
+                    $this->addToBreadcrumbs("Home", baseurl("/"));
+                    $this->addToBreadcrumbs("Register");
+                    $this->addToBreadcrumbs(Info);
+                    $this->render("info","info.tpl");
+                } else {
+                    $wrongCode = "De validatie code komt niet overeen met de gestuurde code, probeer opnieuw te registreren.";
+                    $this->website->assign("wrongCode", $wrongCode);
+                    $this->addToBreadcrumbs("Home", baseurl("/"));
+                    $this->addToBreadcrumbs("Register");
+                    $this->addToBreadcrumbs(Info);
+                    $this->render("info","info.tpl");
+
+                }
+            }
+        }
     }
-
 }
 
 ?>
--- templates/emailVerification.tpl
+++ templates/emailVerification.tpl
@@ -1,12 +1,13 @@
 {include file="header.tpl" extracss=""}
 
 <div class="col-md-8 col-sm-8 col-md-offset-2 col-sm-offset-2">
-    <div class="col-md-4 col-sm-4"
-    <form action="Register/" method="post">
+    <div class="col-md-4 col-sm-4">
+    <form action="{baseurl url="Register/Info"}" method="post">
         <label for="emailVerificationCode">Ge-e-mailde code:</label>
-        <input type="text" name="emailVerificationCode" class="form-control" required>
-        <button class="btn btn-default" name="">Check verification code</button>
+        <input type="text" name="emailVerificationCode" class="form-control" required><br>
+        <button class="btn btn-default" type="submit" name="validateCode">Check verification code</button>
     </form>
+    </div>
 </div>
 
 {include file="footer.tpl" extracss=""}
