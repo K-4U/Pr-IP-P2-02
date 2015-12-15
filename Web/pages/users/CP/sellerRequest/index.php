9ec84f3a678680414f88a566b240101caf26c9b0
116808apd <116808apd@gmail.com>
2015-12-15 16:23:04 +0100

    Current progress on seller request. PHP done, busy with styling the pages.

--- Web/pages/users/CP/sellerRequest/index.php
+++ Web/pages/users/CP/sellerRequest/index.php
@@ -5,28 +5,51 @@ class usersCPsellerRequest extends cmsPage {
     function parse() {
 
         if($this->user->isLoggedIn()) {
-            $userInfoArray = array();
-            if(isset($_POST ['submit_1'])) {
-                if(isset($_POST['banknumber'])) {
-                    if((int)$_POST['banknumber'] > 0) {
-                        $userInfoArray['bank_number'] = (int)$_POST['banknumber'];
-                    } else {
-                        $errorMsg = 'Uw banknummer klopt niet';
+            $pageToShow = 0;
+            //Page 0 is default.
+            if(isset($_POST ['submit_1']) && isset($_POST['verificationType']) && isset($_POST['banknumber'])) {
+                if((int)$_POST['banknumber'] > 0) {
+                    $_SESSION['bank_number'] = $_POST['banknumber'];
+
+                } else {
+                    $errorMsg = 'Uw banknummer klopt niet';
+                    $pageToShow = 0;
+                }
+                $_SESSION['security_type'] = $_POST['verificationType'];
+                if(isset($_SESSION['bank_number']) AND isset($_SESSION['security_type'])) {
+                    if(($_POST['verificationType']) == 0)
+                        $pageToShow = 1;
+                    else {
+                        $pageToShow = 2;
                     }
                 }
-                if(isset($_POST['verificationType'])) {
-                    $userInfoArray['security_type'] = (boolean)$_POST['verificationType'];
+            } else {
+                $userInfoArray = array();
+                $userLetterInfo = array();
+                if(isset($_SESSION['bank_number']) AND isset($_SESSION['security_type'])) {
+                    $userInfoArray['username'] = $this->user->getName();
+                    $userInfoArray['bank_number'] = (int)$_SESSION['bank_number'] ;
+                    $userInfoArray['security_type'] = (boolean)$_SESSION['security_type'];
                 }
+                if(isset($_POST['submit_2'])) {
+                    $userInfoArray['creditcard_number'] = (int)$_POST['creditcard'];
+                    $pageToShow = 3;
+                }
+                if(isset($_POST['submit_3'])) {
+                    $userLetterInfo['username'] = $this->user->getName();
+                    $userLetterInfo['key'] = substr(md5($this->user->getName() . date('U')), 10);
+                    $pageToShow = 3;
+                }
+                var_dump($userInfoArray);
+                var_dump($userLetterInfo);
+                unset($_SESSION['security_type']);
+                unset($_SESSION['bank_number']);
             }
-            if(isset($_Post['submit_1'])) {
 
-            }
-            var_dump($userInfoArray);
-
-            else {
-                $this->render("sellerRequest", "users/sellerRequest.tpl");
-            }
+            $this->website->assign("page", $pageToShow);
+            $this->render("sellerRequest", "users/sellerRequest.tpl");
         } else {
+
             $this->website->assign("loginError", "U moet ingelogd zijn voordat u een verkopersaccount kunt aanmaken.");
             $this->render("login", "users/login.tpl");
         }
