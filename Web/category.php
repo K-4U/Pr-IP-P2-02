904ecb6d44e770a3d34b0c2b79acf860bc3e3b38
Koen Beckers <koen@k-4u.nl>
2015-12-16 15:40:58 +0100

    PAGINATION! \o/

--- Web/category.php
+++ Web/category.php
@@ -2,7 +2,7 @@
 include("header.php");
 
 //Register the variables needed from GET
-$varsToRegister = Array("id"=>"integer","args"=>"string");
+$varsToRegister = Array("id"=>"integer","page"=>"integer");
 foreach($varsToRegister as $var=>$type){
     if(array_key_exists($var,$_GET)){
         $$var = $_GET[$var];
@@ -15,11 +15,9 @@ foreach($varsToRegister as $var=>$type){
     }
 }
 
-//Parse args:
-$argsRaw = explode('/', $args);
-
+$argsRaw = Array();
 include("pages/category/index.php");
 $tClass = new categories($website, $user, $argsRaw, $db);
-$tClass->parse($id);
+$tClass->parse($id, $page);
 
 ?>
