8148253e8540e68adb25139add191b5989bdcaee
116808apd <116808apd@gmail.com>
2015-12-16 11:32:49 +0100

    Seller account request pages done.

--- Web/pages/users/CP/sellerRequest/index.php
+++ Web/pages/users/CP/sellerRequest/index.php
@@ -5,48 +5,79 @@ class usersCPsellerRequest extends cmsPage {
     function parse() {
 
         if($this->user->isLoggedIn()) {
-            $pageToShow = 0;
-            //Page 0 is default.
-            if(isset($_POST ['submit_1']) && isset($_POST['verificationType']) && isset($_POST['banknumber'])) {
-                if((int)$_POST['banknumber'] > 0) {
-                    $_SESSION['bank_number'] = $_POST['banknumber'];
+            $sql = "SELECT username FROM sellers WHERE username=%s";
+            $userResult = $this->db->buildQuery($sql, $this->user->getName());
+            if($this->db->getHasRows($userResult)) {
+                $errorMsg = "U bent al een verkoper of er is al een verzoek verstuurd";
+                $pageToShow = -1;
 
+            } else {
+                $pageToShow = 0;
+                //Page 0 is default.
+//            && isset($_POST['verificationType']) && isset($_POST['banknumber']
+                if(isset($_POST ['submit_1'])) {
+                    if(strlen($_POST['banknumber']) > 0) {
+                        $_SESSION['bank_number'] = $_POST['banknumber'];
+                    } else {
+                        $errorMsg = 'Uw banknummer klopt niet';
+                        $pageToShow = 0;
+                    }
+                    $_SESSION['security_type'] = $_POST['verificationType'];
+                    if(isset($_SESSION['bank_number']) AND isset($_SESSION['security_type'])) {
+                        if(($_POST['verificationType']) == 0)
+                            $pageToShow = 1;
+                        else {
+                            $pageToShow = 2;
+                        }
+                    } else {
+                        $errorMsg = "U bent vergeten een verificatie optie te kiezen.";
+                    }
                 } else {
-                    $errorMsg = 'Uw banknummer klopt niet';
-                    $pageToShow = 0;
-                }
-                $_SESSION['security_type'] = $_POST['verificationType'];
-                if(isset($_SESSION['bank_number']) AND isset($_SESSION['security_type'])) {
-                    if(($_POST['verificationType']) == 0)
-                        $pageToShow = 1;
-                    else {
-                        $pageToShow = 2;
+                    $userInfoArray = array();
+                    $userLetterInfo = array();
+                    if(isset($_SESSION['bank_number']) AND isset($_SESSION['security_type'])) {
+                        if(!preg_match('/[a-zA-Z]{2}[0-9]{2}[a-zA-Z0-9]{4}[0-9]{7}([a-zA-Z0-9]?){0,16}/', $_SESSION['bank_number'])) {
+                            $errorMsg = "Uw IBAN klopt niet. Controleer of uw IBAN de correcte tekens bevat en of alle spaties verwijderd zijn.";
+                        }
+
+                    }
+                    if(isset($_POST['submit_2'])) {
+                        if(preg_match('/^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$/', $_POST['creditcard'])) {
+                            $userInfoArray['username'] = $this->user->getName();
+                            $userInfoArray['bank_number'] = str_replace(" ", "", $_SESSION['bank_number']);
+                            $userInfoArray['security_type'] = (boolean)$_SESSION['security_type'];
+                            $userInfoArray['creditcard_number'] = $_POST['creditcard'];
+                            $pageToShow = 3;
+
+                            unset($_SESSION['security_type']);
+                            unset($_SESSION['bank_number']);
+
+                            $this->db->insert("sellers", $userInfoArray);
+                        } else {
+                            $errorMsg = "Uw creditcard nummer klopt niet.";
+                            $pageToShow = 1;
+                        }
+                    }
+                    if(isset($_POST['submit_3'])) {
+                        $userInfoArray['username'] = $this->user->getName();
+                        $userInfoArray['bank_number'] = str_replace(" ", "", $_SESSION['bank_number']);
+                        $userInfoArray['security_type'] = (boolean)$_SESSION['security_type'];
+                        $userLetterInfo['username'] = $this->user->getName();
+                        $userLetterInfo['verification_key'] = substr(md5($this->user->getName() . date('U')), 1, 10);
+                        $pageToShow = 3;
+
+                        unset($_SESSION['security_type']);
+                        unset($_SESSION['bank_number']);
+
+                        $this->db->insert("sellers", $userInfoArray);
+                        $this->db->insert("verifications", $userLetterInfo);
+                        var_dump($this->db->getLastError());
                     }
                 }
-            } else {
-                $userInfoArray = array();
-                $userLetterInfo = array();
-                if(isset($_SESSION['bank_number']) AND isset($_SESSION['security_type'])) {
-                    $userInfoArray['username'] = $this->user->getName();
-                    $userInfoArray['bank_number'] = (int)$_SESSION['bank_number'] ;
-                    $userInfoArray['security_type'] = (boolean)$_SESSION['security_type'];
-                }
-                if(isset($_POST['submit_2'])) {
-                    $userInfoArray['creditcard_number'] = (int)$_POST['creditcard'];
-                    $pageToShow = 3;
-                }
-                if(isset($_POST['submit_3'])) {
-                    $userLetterInfo['username'] = $this->user->getName();
-                    $userLetterInfo['key'] = substr(md5($this->user->getName() . date('U')), 10);
-                    $pageToShow = 3;
-                }
-                var_dump($userInfoArray);
-                var_dump($userLetterInfo);
-                unset($_SESSION['security_type']);
-                unset($_SESSION['bank_number']);
             }
 
             $this->website->assign("page", $pageToShow);
+            $this->website->assign("errorMsg", $errorMsg);
             $this->render("sellerRequest", "users/sellerRequest.tpl");
         } else {
 
--- Web/templates/users/sellerRequest.tpl
+++ Web/templates/users/sellerRequest.tpl
@@ -1,22 +1,42 @@
 {include file="header.tpl" extracss="ucp.css"}
 
-<div class="row">
-    <div class="col-sm-9 col-sm-offset-3 col-md-8 col-md-offset-2 main">
-        <p> HALLO </p>
-    </div>
-</div>
+{if $errorMsg}
+    <div class=" alert alert-danger col-md-8 col-sm-8 col-md-offset-2 col-sm-offset-2">
+        {$errorMsg} </div>
+{/if}
 
+{if $page == -1}
+    <div class="row">
+        <div class="col-sm-9 col-sm-offset-3 col-md-8 col-md-offset-2 main well">
+            <div class="row">
+                <div class="col-md-6 col-sm-6 col-md-offset-1">
+                    <p>
+                        Klik hier om terug te gaan naar het User Control Panel.
+                    </p>
+                </div>
+            </div>
+            <br>
+            <form action="{baseurl url="Users/CP"}">
+                <div class="row">
+                    <div class="col-md-6 col-sm-6 col-md-offset-1">
+                        <input type = "submit" value = " Terug " class="btn btn-cta">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+{/if}
 
 {if $page == 0}
     <div class="row">
-        <div class="col-sm-9 col-sm-offset-3 col-md-8 col-md-offset-2 main">
+        <div class="col-sm-9 col-sm-offset-3 col-md-8 col-md-offset-2 main well">
             <form action="{$self}" method="post" class="form-inline">
                 <div class="row">
                     <div class="col-md-2 col-sm-3 col-md-offset-1">
                         <label for="banknummer" class="labelsData">Rekeningnummer:</label>
                     </div>
                     <div class="cold-md-7">
-                        <input type="text" id="banknumber" name="banknumber" class="form-control">
+                        <input type="text" id="banknumber" name="banknumber" placeholder="NL00 AAAA 0000 0000 00" class="form-control" pattern="{literal}[a-zA-Z]{2}[0-9]{2}[a-zA-Z0-9]{4}[0-9]{7}([a-zA-Z0-9]?){0,16}{/literal}">
                     </div>
                 </div>
                 <br>
@@ -43,34 +63,74 @@
 
 {if $page == 1}
     <div class="row">
-        <div class="col-sm-9 col-sm-offset-3 col-md-8 col-md-offset-2 main">
-            <form action="{$self}" method="post">
-                <label for="creditcard" class="col-md-4 col-sm-4 labelsData">Creditcard:</label>
-                <div class="col-md-8 col-sm-8">
-                    <input type="text" id="creditcard" name="creditcard" class="form-control">
+        <div class="col-sm-9 col-sm-offset-3 col-md-8 col-md-offset-2 main well">
+            <form action="{$self}" method="post" class="form-inline">
+                <div class="row">
+                    <div class="col-md-2 col-sm-3 col-md-offset-1">
+                        <label for="creditcard" class="labelsData">Creditcard:</label>
+                    </div>
+                    <div class="cold-md-7">
+                        <input type="text" id="creditcard" name="creditcard" class="form-control">
+                    </div>
                 </div>
-                <button class="btn btn-cta" type="submit" name="submit_2">Volgende Stap</button>
+                <br>
+                <div class="row">
+                    <div class="col-md-6 col-sm-6 col-md-offset-1">
+                        <button class="btn btn-cta" type="submit" name="submit_2">Volgende Stap</button>
+                    </div>
+                </div>
             </form>
         </div>
     </div>
 {/if}
 
 {if $page == 2}
-    <p>
-        Ja, ik wil mijn adres per post verifiëren.
-        Stuur mij een brief met een authenticatiecode.
-    </p>
-    <form action="{$self}" method="post">
-        <button class="btn btn-cta" type="submit" name="submit_3">Volgende Stap</button>
-    </form>
+    <div class="row">
+        <div class="col-sm-9 col-sm-offset-3 col-md-8 col-md-offset-2 main well">
+            <div class="row">
+                <div class="col-md-6 col-sm-6 col-md-offset-1">
+                    <h3>Adres per post verifiëren</h3>
+                    <p>
+                        Er wordt per post een brief naar het bij ons bekende adres gestuurd.<br>
+                        Hierin staat een activatiecode om uw adres te verifiëren. <br>
+                        Pas wanneer u de code invoert kunt u producten op deze site aanbieden.
+                    </p>
+                </div>
+            </div>
+            <form action="{$self}" method="post">
+                <div class="row">
+                    <div class="col-md-6 col-sm-6 col-md-offset-1">
+                        <button class="btn btn-cta" type="submit" name="submit_3">Volgende Stap</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
 {/if}
 
 {if $page == 3}
- <p>succes</p>
+    <div class="row">
+        <div class="col-sm-9 col-sm-offset-3 col-md-8 col-md-offset-2 main well">
+            <div class="row">
+                <div class="col-md-6 col-sm-6 col-md-offset-1">
+                    <p>
+                        Uw aanvraag is ontvangen. Uw account wordt zo snel mogelijk geupgrade naar een verkopersaccount. <br>
+                        <br>
+                        Als u de optie per post heeft geselecteerd krijgt u binnenkort een brief thuisgestuurd. <br>
+                        In deze brief staat dan de verificatiecode om uw aanvraag af te ronden.
+                    </p>
+                </div>
+            </div>
+            <br>
+            <form action="{baseurl url="Users/CP"}">
+                <div class="row">
+                    <div class="col-md-6 col-sm-6 col-md-offset-1">
+                        <input type = "submit" value = " Afronden " class="btn btn-cta">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
 {/if}
-
-
-{$page}
-
 
 {include file="footer.tpl"}
