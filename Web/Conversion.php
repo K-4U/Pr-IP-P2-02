3534dc259a34676be2f16c36253c182255c34bd0
ALE.Scheps <steak@live.nl>
2016-01-06 16:25:49 +0100

    Conversion script almost done

--- Web/Conversion.php
+++ Web/Conversion.php
@@ -1,15 +1,9 @@
 <?php
-/*****************
- * header.php
- * Has most of the standard functions and includes.
- * This file will be included for every PHP page.
- * Author: Koen Beckers
- * Copyright 2005 - 2013 K-4U
- * Date: 13-10-2013
- *****************/
 include("config.php");
 require("libs/classes/db.class.php");
-global $dbIn, $dbOut;
+global $dbIn, $dbOut, $categoryIds;
+$beginTime = date('U');
+$categoryIds = Array();
 
 $dbIn = new db($config['SQL']['host'], $config['SQL']['user'], $config['SQL']['pass'], "veiling_other");
 $dbIn->setType($config['SQL']['type']);
@@ -22,11 +16,9 @@ $dbOut->setType($config['SQL']['type']);
 $dbOut->connect();
 
 $result = $dbIn->query("SELECT ID, Name, Parent FROM Categorieen");
-$hoi = $dbIn->fetchAssoc($result);
-//var_dump($hoi);
 
-//ORDER BY priority
-
+echo "<pre>";
+echo "Fetching categories\r\n";
 function getCategory($parentId = null){
     global $dbIn;
     if($parentId == null){
@@ -43,38 +35,139 @@ function getCategory($parentId = null){
     return $categories;
 }
 
-$hoihoi = getCategory();
-echo "<pre>";
-var_dump($hoihoi);
+echo "Done fetching.\r\n";
+
+function setCategory($category){
+    global $dbOut, $categoryIds;
+    $priorityNumber = 1;
+
+    $newFormat = Array(
+        "name" => $category['Name'],
+        "parent" => $category['Parent'],
+        "priority" => $category['priority']
+    );
+    $dbOut->insert("categories", $newFormat); //uncomment this to get the categories insert running again!
+
+    $lastInsert = $dbOut->getLastInsertedId();
+    $categoryIds[$category['ID']] = $lastInsert;
+    foreach($category['sub'] as $subCategory){
+        $subCategory['priority'] = $priorityNumber;
+        $priorityNumber ++;
+        $subCategory['Parent'] = $lastInsert;
+        setCategory($subCategory);
+    }
+}
+
+echo "Inserting categories\r\n";
+$priorityNumber = 1;
+$getCategories = getCategory();
+foreach($getCategories as $category){// root categories
+    $category['priority'] = $priorityNumber;
+    $priorityNumber ++;
+    $category['Parent'] = null;
+    setCategory($category);
+}
+
+$categoryInsertTime = (date('U')- $beginTime);
+echo "Done inserting. (took ". $categoryInsertTime ." seconds)\r\n\r\n";
+
+echo "Fetching users.\r\n";
+
+function getUsers(){
+    global $dbIn;
+    $result = $dbIn->query("SELECT Username, Postalcode, Country FROM Users ");
+    $users = $dbIn->fetchAllAssoc($result);
+    return $users;
+}
+
+$usersVar = getUsers();
+echo "Done fetching.\r\n";
+
+echo "Inserting users.\r\n";
+function setUsers($usersArr){
+    global $dbOut;
+
+    $newFormat = Array(
+        "firstname" => "unknown",
+        "lastname" => "person",
+        "adress_street1" => "unknown address",
+        "adress_street2" => null,
+        "adress_number" => "123",
+        "city" => "unknown city",
+        "email" => "batch@batch.batch",
+        "password" => "batch",
+        "security_question" => "1",
+        "security_answer" => "batch",
+        "isseller" => "1",
+        "username" => substr(strlen($usersArr['Username']) < 4 ? "FILL" . $usersArr['Username'] : $usersArr['Username'], 0, 12),
+        "postalcode" => substr($usersArr['Postalcode'] == null ? "0000AB": $usersArr['Postalcode'], 0, 6),
+        "country" => $usersArr['Country'],
+        "birthdate" => "01-01-1990"
+    );
+    $resultUsername = $dbOut->buildQuery("SELECT Username FROM Users WHERE Username=%s", $newFormat['username']);
+    $usernameExist = $dbOut->getHasRows($resultUsername);
+    if(!$usernameExist) {
+        $dbOut->insert("users", $newFormat); //uncomment this to get the users insert running again!
+    }
+}
+
+foreach($usersVar as $user){
+    setUsers($user);
+}
+$usersInsertTime = ((date('U') - $beginTime) - $categoryInsertTime);
+echo "Done inserting users. (took " . $usersInsertTime . " seconds )\r\n\r\n";
+
+echo "Fetching objects\r\n";
+function getObjects(){
+    global $dbIn;
+    $result = $dbIn->query("SELECT * FROM Items");
+    $objects = $dbIn->fetchAllAssoc($result);
+    return $objects;
+}
+$getObjects = getObjects();
+echo "Done fetching objects\r\n";
+
+function setObjects($objectArr){
+    global $dbOut, $dbIn, $categoryIds;
+
+    $newFormat = Array(
+        "title" => substr(strip_tags($objectArr['Titel']), 0, 60),
+        "description" => substr(strip_tags($objectArr['Beschrijving']), 0, 4000),
+        "start_bid" => $objectArr['Prijs'],
+        "payment_method" => "Contant",
+        "city" => "batch",
+        "country" => $objectArr['Land'],
+        "seller" => substr(strlen($objectArr['Verkoper']) < 4 ? "FILL" . $objectArr['Verkoper'] : $objectArr['Verkoper'], 0, 12),
+        "duration" => "3"
+    );
+    $dbOut->insert("objects", $newFormat);
+    $lastInsertedObject = $dbOut->getLastInsertedId();
+
+    $newFormatCatObj = Array(
+        "object_id" => $lastInsertedObject,
+        "category_id" => $categoryIds[$objectArr['Categorie']]
+
+    );
+    $dbOut->insert("object_in_category", $newFormatCatObj);
+
+    $result = $dbIn->buildQuery("SELECT * FROM Illustraties WHERE ItemID = %s", $objectArr['ID']);
+    $getFiles = $dbIn->fetchAssoc($result);
+    $newFormatPic = Array(
+        "filename" => substr($getFiles['IllustratieFile'], 0, 37),
+        "objectid" => $lastInsertedObject
+    );
+    $dbOut->insert("files", $newFormatPic);
+}
+$objectVar = getObjects();
+echo "Inserting objects\r\n";
+foreach($objectVar as $object){
+    setObjects($object);
+}
+$objectsInsertTime = ((date('U') - $beginTime) - $usersInsertTime);
+echo "Done inserting objects. (took ". $objectsInsertTime . " seconds)\r\n\r\n";
+
+$endTime = date('U');
+
+echo "Took " . ($endTime - $beginTime) . " seconds.";
 echo "</pre>";
-//foreach(){
-//
-//    $idGiven;
-//    $priorityGiven;
-//    $idOld;
-//
-//
-//}
-
-//$countResult = $this->db->buildQuery("SELECT COUNT(id) AS c FROM objects WHERE title LIKE '%%s%' OR description LIKE '%%s%'",$query, $query);
-//$c = $this->db->fetchAssoc($countResult)['c'];
-//
-//$sql = "SELECT id,title,end_moment,start_bid FROM objects WHERE title LIKE '%%s%' OR description LIKE '%%s%'";
-//$result = null;
-//$maxPerPage = 18;
-//if($c > $maxPerPage){
-//    if($p > 0){
-//        $fo = $p * $maxPerPage;
-//    }else{
-//        $fo = 0;
-//    }
-//    $fe = $fo + $maxPerPage;
-//    $sql .= " OFFSET %i ROWS FETCH NEXT %i ROWS ONLY;";
-//    $result = $this->db->buildQuery($sql, $query, $query, $fo, $fe);
-//    $this->website->assign("paginationNeeded", true);
-//    $this->website->assign("maxPages", ($c / $maxPerPage)-1);
-//}else{
-//    $result = $this->db->buildQuery($sql, $query, $query);
-//}
-
 ?>
